b2b8c33bd0f9a5549ae4d9f387d886fd73b780e6
Megh <mmeghkapuriya@gmail.com>
2025-12-30 16:10:34 +0530

    AI

---
+++ musicLibrary.py
@@ -0,0 +1,6 @@
+music = {
+    "blue" :"https://www.youtube.com/watch?v=IpFX2vq8HKw&list=RDIpFX2vq8HKw&start_radio=1&pp=ygUEYmx1ZaAHAQ%3D%3D",
+    "F1" :"https://www.youtube.com/watch?v=YhX_Woa3kVA&list=RDYhX_Woa3kVA&start_radio=1&pp=ygUCRjGgBwE%3D",
+    "suzume" :"https://www.youtube.com/watch?v=qal34e9v_pk&list=RDqal34e9v_pk&start_radio=1&pp=ygULc3V6dW1lIHNvbmegBwE%3D",
+    "yourname" :"https://www.youtube.com/watch?v=CYTnZV_NywU&list=RDCYTnZV_NywU&start_radio=1&pp=oAcB"
+    }
